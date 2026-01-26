5859623cce7c471e028e40000cd0c6e192866642
Saravanakumar-2323 <saravanakumar23133@gmail.com>
2026-01-26 23:10:56 +0530

    program commit

---
+++ 3_Digit_Number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+
+    if(a>=100 && a<=999) printf("Three Digit Number");
+    else printf("Not a Three Digit Number");
+    
+    return 0;
+}
