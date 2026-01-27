7708ef523554f550f646f11092a60a8ac639ec95
Saravanakumar-2323 <saravanakumar23133@gmail.com>
2026-01-27 17:18:13 +0530

    initial commit

---
+++ Switch Statement Problems/Mobile_Recharge.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int plan, payment;
+    printf("Plan(1/2): ");
+    scanf("%d", &plan);
+    printf("Payment Mode(11/12/13): ");
+    scanf("%d", &payment);
+
+    switch (plan) {
+        case 1:
+            switch (payment) {
+                case 11:
+                case 12:
+                    printf("Pay Rs.179");
+                    break;
+                case 13:
+                    printf("Pay Rs.199");
+                    break;
+                default:
+                    printf("Unavailable Option");
+            }
+            break;
+
+        case 2:
+            switch (payment) {
+                case 11:
+                case 12:
+                    printf("Pay Rs.379");
+                    break;
+                case 13:
+                    printf("Pay Rs.399");
+                    break;
+                default:
+                    printf("Unavailable Option");
+            }
+            break;  
+
+        default:
+            printf("Unavailable Plan");
+    }
+    return 0;
+}
