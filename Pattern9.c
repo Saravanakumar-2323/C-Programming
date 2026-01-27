26c41868333a385493e7e85f4bda0efafacb22b3
Saravanakumar-2323 <saravanakumar23133@gmail.com>
2026-01-27 12:13:21 +0530

    initial commit

---
+++ Pattern9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int end = n*2-1;
+    for(int i=1;i<=end;i++){
+        for(int j=1;j<=i;j++){
+            if(i%2!=0)
+                printf("*");
+        }
+        printf("\n");
+    }
+
+    for(int i=end-1;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            if(i%2!=0)
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
