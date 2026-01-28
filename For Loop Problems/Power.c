ef26b50f36864aa12bef62a4ab2c8c70f074722d
Saravanakumar-2323 <saravanakumar23133@gmail.com>
2026-01-28 15:18:57 +0530

    initial commit

---
+++ For Loop Problems/Power.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int n,p;
+    scanf("%d %d",&n,&p);
+    int result = 1;
+    for(int i=1;i<=p;i++){
+        result = result * n;
+    }
+    printf("%d",result);
+}
