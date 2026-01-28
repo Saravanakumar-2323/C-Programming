af0fdac740590904fd59cb164c44ea08726b1672
Saravanakumar-2323 <saravanakumar23133@gmail.com>
2026-01-28 09:49:51 +0530

    initial commit

--- For Loop Problems/Even_Numbers.c
+++ For Loop Problems/Even_Numbers.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main() {
     int n;
-    int sum = 0;
     scanf("%d",&n);
 
     for(int i=1;i<=n;i++){
-        sum += i;
+        if(i%2==0)
+            printf("%d ",i);
     }
-    printf("%d",sum);
     return 0;
 }
