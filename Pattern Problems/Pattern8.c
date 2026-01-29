7e4737ece303c11e13a5478ec2ae1378c87e0304
Saravanakumar-2323 <saravanakumar23133@gmail.com>
2026-01-29 15:51:49 +0530

    initial commit

--- Pattern Problems/Pattern8.c
+++ Pattern Problems/Pattern8.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main() {
-    int n;
-    scanf("%d",&n);
-    int end = n*2-1;
+    int n=5;
+    for(int i=n;i>=1;i--){
 
-    for(int i=end;i>=1;i--){
-        for(int j=1;j<=i;j++){
-            if(i%2!=0)
-                printf("*");
+        for(int s=1;s<=n-i;s++){
+                printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
         }
         printf("\n");
     }
--- Pattern Problems/Pattern9.c
+++ Pattern Problems/Pattern9.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 int main() {
-    int n;
-    scanf("%d",&n);
-    int end = n*2-1;
-    for(int i=1;i<=end;i++){
-        for(int j=1;j<=i;j++){
-            if(i%2!=0)
-                printf("*");
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
         }
         printf("\n");
     }
-
-    for(int i=end-1;i>=1;i--){
-        for(int j=1;j<=i;j++){
-            if(i%2!=0)
-                printf("*");
+    
+    for(int i=n;i>=1;i--){
+        for(int s=1;s<=n-i;s++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
         }
         printf("\n");
     }
